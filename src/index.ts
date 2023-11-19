c0a5b296d253ce521b2fb42eaf326db26cc7e671
killian-sweeney <killianmsweeney@gmail.com>
2023-11-19 16:52:56 -0500

    init commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * @function add
+ * @param {number} a the first number
+ * @param {number} b the second numnber
+ * @returns reutrns the sum of the numbers.
+ */
+export function add(a: number, b: number): number {
+	return a + b;
+  }
+  
+  console.log(add(3, 5)); //output: 8
+
+// YOUR GOAL:
+
+  // import Loop from "./src/Loop.ts";
+  // import FieldMap from "./src/FieldMap.ts";
+  // import LoopMap from "./src/LoopMap.ts";
+  // import Transaction from "./src/Transaction.ts";
+  
+  // /**
+  //  * @module index
+  //  * @description The index module exports the following classes:
+  //  * @description - Loop
+  //  * @description - FieldMap
+  //  * @description - LoopMap
+  //  * @description - Transaction
+  //  * @example
+  //  * import { Loop, FieldMap, LoopMap, Transaction } from "edi-file-utility";
+  //  */
+  // export { Loop, FieldMap, LoopMap, Transaction };

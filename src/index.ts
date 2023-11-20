529d390cc423de131e6151801d26950ba1877211
killian-sweeney <killianmsweeney@gmail.com>
2023-11-19 21:17:49 -0500

    prep for testing

--- src/index.ts
+++ src/index.ts
@@ -4,27 +4,20 @@
  * @param {number} b the second numnber
  * @returns reutrns the sum of the numbers.
  */
-export function add(a: number, b: number): number {
-	return a + b;
-  }
-  
-  console.log(add(3, 5)); //output: 8
 
-// YOUR GOAL:
 
-  // import Loop from "./src/Loop.ts";
-  // import FieldMap from "./src/FieldMap.ts";
-  // import LoopMap from "./src/LoopMap.ts";
-  // import Transaction from "./src/Transaction.ts";
-  
-  // /**
-  //  * @module index
-  //  * @description The index module exports the following classes:
-  //  * @description - Loop
-  //  * @description - FieldMap
-  //  * @description - LoopMap
-  //  * @description - Transaction
-  //  * @example
-  //  * import { Loop, FieldMap, LoopMap, Transaction } from "edi-file-utility";
-  //  */
-  // export { Loop, FieldMap, LoopMap, Transaction };
+import Loop from "./types/main/Loop";
+import FieldMap from "./types/main/FieldMap";
+import LoopMap from "./types/main/LoopMap";
+import Transaction from "./types/main/Transaction";
+
+/**
+ * @module index
+ * @description The index module exports the following classes:
+ * @description - Loop
+ * @description - FieldMap
+ * @description - LoopMap
+ * @description - Transaction
+ * @example
+ */
+export { Loop, FieldMap, LoopMap, Transaction };

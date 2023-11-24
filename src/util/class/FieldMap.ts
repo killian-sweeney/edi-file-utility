db9fdc14bcddd377e7d675650058926263d31012
killian-sweeney <killianmsweeney@gmail.com>
2023-11-24 17:19:52 -0500

    update FieldMap docs

--- src/util/class/FieldMap.ts
+++ src/util/class/FieldMap.ts
@@ -1,51 +1,41 @@
 /**
  * @class FieldMap
  * @description Used to declare the position of a Field in a segment to a json object key value.
- * @param {Object} options
- * @param {String} options.segmentIdentifier The segment identifier.
- * @param {String} options.identifierValue The value of the field identifier.
- * @param {Number} options.identifierPosition The position of the field identifier.
- * @param {Number} options.valuePosition The position of the field value.
  * @example
- * const fieldMap = new FieldMap({
- *  segmentIdentifier: "INS",
- * identifierValue: null,
- * identifierPosition: null,
- * valuePosition: 1,
- * });
- * @example
- * const newObjectToMapFieldsTo = {
- *  subscriberNumber: new FieldMap({
- *    segmentIdentifier: "REF",
- *    identifierValue: "0F",
- *    identifierPosition: null,
- *   valuePosition: 1,
- *  }),
- *  groupId: new FieldMap({
- *    segmentIdentifier: "REF",
- *    identifierValue: 1L,
- *    identifierPosition: null,
- *    valuePosition: 7,
- *  }),
- * };
+ * let example = {
+ * 	subscriberIndicator: new FieldMap("INS",null, null, 0),
+ * 	subscriberNumber:    new FieldMap("REF","0F", 0   , 1)
+ * }
+ * console.log(example)
+ * // {
+ * //   subscriberIndicator: FieldMap {
+ * //     segmentIdentifier: 'INS',
+ * //     identifierValue: null,
+ * //     identifierPosition: null,
+ * //     valuePosition: 0
+ * //   },
+ * //   subscriberNumber: FieldMap {
+ * //     segmentIdentifier: 'REF',
+ * //     identifierValue: '0F',
+ * //     identifierPosition: 0,
+ * //     valuePosition: 1
+ * //   }
+ * // }
  */
-
 export default class FieldMap {
 
 	constructor(
 		public segmentIdentifier: string,
-		public identifierValue: string | null = null,
-		public identifierPosition: number | null = null,
+		public identifierValue: string | null, // = null,
+		public identifierPosition: number | null,// = null,
 		public valuePosition: number
 	) {	}
 
 	/**
-	 * @method toJSON
 	 * @description Returns a JSON representation of the FieldMap.
-	 * @returns {Object}
-	 * @memberof FieldMap
+	 * @returns {string}
 	 * @example
-	 * const json = fieldMap.toJSON();
+	 * const json = example.subscriberIndicator.toJson();
 	 * console.log(json);
 	 * // {
 	 * //   segmentIdentifier: "INS",
@@ -54,12 +44,18 @@ export default class FieldMap {
 	 * //   valuePosition: 1,
 	 * // }
 	 */
-	toJSON(): object {
-		return {
-			segmentIdentifier: this.segmentIdentifier,
-			identifierValue: this.identifierValue,
-			identifierPosition: this.identifierPosition,
-			valuePosition: this.valuePosition,
-		};
+	toJson(): string {
+		return JSON.stringify(this) 
+		// {
+		// 	segmentIdentifier: this.segmentIdentifier,
+		// 	identifierValue: this.identifierValue,
+		// 	identifierPosition: this.identifierPosition,
+		// 	valuePosition: this.valuePosition,
+		// };
 	}
 }
+let example = {
+ 	subscriberIndicator: new FieldMap("INS",null, null, 0),
+ 	subscriberNumber:    new FieldMap("REF","0F", 0   , 1)
+ }
+console.log(example.subscriberIndicator.toJson())

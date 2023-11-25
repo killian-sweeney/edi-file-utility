3bce8f72ab80505e809cb7b3ddde3ddc1c03a2ba
killian-sweeney <killianmsweeney@gmail.com>
2023-11-24 20:51:24 -0500

    mapping bug roadblock

--- src/util/class/Transaction.ts
+++ src/util/class/Transaction.ts
@@ -600,3 +600,328 @@ export default class Transaction {
 		});
 	}
 }
+
+// ----------------------------------------------------------------------------------------------------------
+
+import * as fs from 'fs';
+
+
+const file = fs.readFileSync("./bin/proto/input/834.edi", 'utf8');
+// console.log(file)
+// ISA*00*          *00*          *ZZ*123456789012345*ZZ*123456789012346*080503*1705*>*00501*000010216*0*T*:~
+// GS*BE*1234567890*1234567890*20080503*1705*20213*X*005010X220A1~
+// ST*834*12345*005010X220A1~
+// BGN*00*12456*19980520*1200****2~
+// N1*P5**FI*999888777~
+// N1*IN**FI*654456654~
+// INS*Y*18*030*28*A***FT~
+// REF*0F*ABCXYZ~
+// REF*1L*GROUP2023~
+// REF*DX*0001~
+// REF*ZZ*00~                               
+// DTP*336*D8*20220601~
+// NM1*IL*1*LAKIN*CARY****34*999681271~
+// PER*IP**HP*7172343334*EM*email@ex.com~
+// N3*734 DARE BRIDGE~
+// N4*TAMPA*FL*33618**CY*HILLSBOROUGH~
+// DMG*D8*19880306*F~
+// HD*030**HLT*PLAN_ID*EMP~
+// DTP*348*D8*20230101~
+// INS*Y*18*030*28*A***FT~
+// REF*0F*ABCXYZ~
+// REF*1L*GROUP2023~
+// REF*DX*0001~
+// REF*ZZ*00~
+// DTP*336*D8*20220601~
+// NM1*IL*1*LAKIN*CARY****34*999681271~
+// PER*IP**HP*7172343334*EM*email@ex.com~
+// N3*734 DARE BRIDGE~
+// N4*TAMPA*FL*33618**CY*HILLSBOROUGH~
+// DMG*D8*19880306*F~
+// HD*030**HLT*PLAN_ID*EMP~
+// DTP*348*D8*20230101~
+// SE*21*12345~
+// GE*1*20213~
+// IEA*1*000010216~
+
+const transaction = new Transaction();
+transaction.generateSegments(file);
+// console.log(transaction)
+	// Transaction {
+	// 	segments: [
+	// 	  Segment { name: 'ISA', fields: [Array] },
+	// 	  Segment { name: 'GS', fields: [Array] },
+	// 	  Segment { name: 'ST', fields: [Array] },
+	// 	  Segment { name: 'BGN', fields: [Array] },
+	// 	  Segment { name: 'N1', fields: [Array] },
+	// 	  Segment { name: 'N1', fields: [Array] },
+	// 	  Segment { name: 'INS', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'DTP', fields: [Array] },
+	// 	  Segment { name: 'NM1', fields: [Array] },
+	// 	  Segment { name: 'PER', fields: [Array] },
+	// 	  Segment { name: 'N3', fields: [Array] },
+	// 	  Segment { name: 'N4', fields: [Array] },
+	// 	  Segment { name: 'DMG', fields: [Array] },
+	// 	  Segment { name: 'HD', fields: [Array] },
+	// 	  Segment { name: 'DTP', fields: [Array] },
+	// 	  Segment { name: 'INS', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'REF', fields: [Array] },
+	// 	  Segment { name: 'DTP', fields: [Array] },
+	// 	  Segment { name: 'NM1', fields: [Array] },
+	// 	  Segment { name: 'PER', fields: [Array] },
+	// 	  Segment { name: 'N3', fields: [Array] },
+	// 	  Segment { name: 'N4', fields: [Array] },
+	// 	  Segment { name: 'DMG', fields: [Array] },
+	// 	  Segment { name: 'HD', fields: [Array] },
+	// 	  Segment { name: 'DTP', fields: [Array] },
+	// 	  Segment { name: 'SE', fields: [Array] },
+	// 	  Segment { name: 'GE', fields: [Array] },
+	// 	  Segment { name: 'IEA', fields: [Array] }
+	// 	],
+	// 	loops: [],
+	// 	debug: false
+	//   }
+// console.log(transaction.segments)
+	// [
+	// 	Segment {
+	// 	name: 'ISA',
+	// 	fields: [
+	// 		[Field], [Field], [Field],
+	// 		[Field], [Field], [Field],
+	// 		[Field], [Field], [Field],
+	// 		[Field], [Field], [Field],
+	// 		[Field], [Field], [Field],
+	// 		[Field]
+	// 	]
+	// 	},
+	// 	Segment {
+	// 	name: 'GS',
+	// 	fields: [
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field]
+	// 	]
+	// 	},
+	// 	Segment { name: 'ST', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment {
+	// 	name: 'BGN',
+	// 	fields: [
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field]
+	// 	]
+	// 	},
+	// 	Segment {
+	// 	name: 'N1',
+	// 	fields: [ [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment {
+	// 	name: 'N1',
+	// 	fields: [ [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment {
+	// 	name: 'INS',
+	// 	fields: [
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field]
+	// 	]
+	// 	},
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'DTP', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment {
+	// 	name: 'NM1',
+	// 	fields: [
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field]
+	// 	]
+	// 	},
+	// 	Segment {
+	// 	name: 'PER',
+	// 	fields: [ [Field], [Field], [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment { name: 'N3', fields: [ [Field] ] },
+	// 	Segment {
+	// 	name: 'N4',
+	// 	fields: [ [Field], [Field], [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment { name: 'DMG', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment {
+	// 	name: 'HD',
+	// 	fields: [ [Field], [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment { name: 'DTP', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment {
+	// 	name: 'INS',
+	// 	fields: [
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field]
+	// 	]
+	// 	},
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'REF', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'DTP', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment {
+	// 	name: 'NM1',
+	// 	fields: [
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field], [Field],
+	// 		[Field]
+	// 	]
+	// 	},
+	// 	Segment {
+	// 	name: 'PER',
+	// 	fields: [ [Field], [Field], [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment { name: 'N3', fields: [ [Field] ] },
+	// 	Segment {
+	// 	name: 'N4',
+	// 	fields: [ [Field], [Field], [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment { name: 'DMG', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment {
+	// 	name: 'HD',
+	// 	fields: [ [Field], [Field], [Field], [Field], [Field] ]
+	// 	},
+	// 	Segment { name: 'DTP', fields: [ [Field], [Field], [Field] ] },
+	// 	Segment { name: 'SE', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'GE', fields: [ [Field], [Field] ] },
+	// 	Segment { name: 'IEA', fields: [ [Field], [Field] ] }
+	// ]
+
+
+const itemLoop = new Loop();
+itemLoop.setPosition(0);
+
+itemLoop.addSegmentIdentifiers(["INS", "REF", "REF", "DTP", "DTP", "NM1", "PER", "N3", "N4", "DMG", "HD", "DTP", "DTP"]);
+
+transaction.addLoop(itemLoop);
+// console.log(transaction.loops)
+// [
+// 	Loop {
+// 	  position: 0,
+// 	  segmentIdentifiers: [
+// 		'INS', 'REF', 'REF',
+// 		'DTP', 'DTP', 'NM1',
+// 		'PER', 'N3',  'N4',
+// 		'DMG', 'HD',  'DTP',
+// 		'DTP'
+// 	  ],
+// 	  contents: [ [] ]
+// 	}
+// ]
+
+transaction.runLoops();
+// console.log(transaction)
+
+// console.log(transaction.segments[0])
+	// Segment {
+	// 	name: 'ISA',
+	// 	fields: [
+	// 	  Field { element: '00' },
+	// 	  Field { element: '' },
+	// 	  Field { element: '00' },
+	// 	  Field { element: '' },
+	// 	  Field { element: 'ZZ' },
+	// 	  Field { element: '123456789012345' },
+	// 	  Field { element: 'ZZ' },
+	// 	  Field { element: '123456789012346' },
+	// 	  Field { element: '080503' },
+	// 	  Field { element: '1705' },
+	// 	  Field { element: '>' },
+	// 	  Field { element: '00501' },
+	// 	  Field { element: '000010216' },
+	// 	  Field { element: '0' },
+	// 	  Field { element: 'T' },
+	// 	  Field { element: ':' }
+	// 	]
+	//   }
+
+let authorizationInformationQualifier = new FieldMap("INS", null, null,  0);
+const mapLogic1 = authorizationInformationQualifier
+
+const mapLogic = {
+	header: {
+		ISA: {
+			authorizationInformationQualifier: 		new FieldMap("INS", null, null,  0),
+			authorizationInformation: 				new FieldMap("INS", null, null,  1),
+			securityInformationQualifier: 			new FieldMap("INS", null, null,  2),
+			securityInformation: 					new FieldMap("INS", null, null,  3),
+			interchangeSenderIdQualifier: 			new FieldMap("INS", null, null,  4),
+			interchangeSenderId: 					new FieldMap("INS", null, null,  5),
+			interchangeReceiverIdQualifier: 		new FieldMap("INS", null, null,  6),
+			interchangeReceiverId: 					new FieldMap("INS", null, null,  7),
+			interchangeDate: 						new FieldMap("INS", null, null,  8),
+			interchangeTime: 						new FieldMap("INS", null, null,  9),
+			interchangeControlStandardsIdentifier: 	new FieldMap("INS", null, null,  0),
+			interchangeControlVersionNumber: 		new FieldMap("INS", null, null, 11),
+			interchangeControlNumber: 				new FieldMap("INS", null, null, 12),
+			acknowledgmentRequested: 				new FieldMap("INS", null, null, 13),
+			usageIndicator: 						new FieldMap("INS", null, null, 14),
+			componentElementSeparator:				new FieldMap("INS", null, null, 15),
+		},
+		GS: {
+
+		},
+		ST: {
+
+		},
+		BGN: {
+
+		}
+	},
+	detail: {
+		member: new LoopMap(
+			0,
+			{
+				subscriberIndicator: 		new FieldMap("INS", null, null,  0),
+				individualRelationshipCode: new FieldMap("INS", null, null,  1),
+				insuredMaintenanceTypeCode: new FieldMap("INS", null, null,  2),
+				maintenanceReasonCode: 		new FieldMap("INS", null, null,  3),
+				benefitStatusCode:			new FieldMap("INS", null, null,  4),
+				medicarePlanCode: 			new FieldMap("INS", null, null,  5),
+				cobraQualifyingEventCode: 	new FieldMap("INS", null, null,  6),
+				employmentStatusCode: 		new FieldMap("INS", null, null,  7),
+				studentStatusCode: 			new FieldMap("INS", null, null,  8),
+				handicapIndicator: 			new FieldMap("INS", null, null,  9),
+				deathDateFormatQualifier: 	new FieldMap("INS", null, null,  0),
+				insuredIndividualDeathDate: new FieldMap("INS", null, null, 11),
+				confidentialityCodeNotUsed: new FieldMap("INS", null, null, 12),
+				cityNotUsed: 				new FieldMap("INS", null, null, 13),
+				stateCodeNotUsed: 			new FieldMap("INS", null, null, 14),
+				countryCodeNotUsed: 		new FieldMap("INS", null, null, 15),
+				birthSequenceNumber:		new FieldMap("INS", null, null, 16),
+			}
+		)
+	}
+}
+console.log(mapLogic.detail.member.values)
+
+// const jsonMapLogic = JSON.stringify(mapLogic)
+
+// console.log(transaction.getSegments())
+
+const output = transaction.mapSegments(mapLogic1, transaction.getSegments());
+console.log(JSON.stringify(output))

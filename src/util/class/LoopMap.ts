553f6480a8533c678ad272db60b17759ba01b7e3
killian-sweeney <killianmsweeney@gmail.com>
2023-11-24 19:17:56 -0500

    add LoopMap

--- src/util/class/LoopMap.ts
+++ src/util/class/LoopMap.ts
@@ -1,35 +1,35 @@
+import FieldMap from "./FieldMap"
+
 /**
  * @class LoopMap
  * @description A LoopMap is used to map a Loop to the specified object key value in an array.
- * @param {Object} options
- * @param {Number} options.position The position of the Loop in the transaction.
- * @param {Object} options.values An object containing FieldMaps.
  * @example
- * const objectToMapKeyValuesTo = {
- * header: {
- *  // header field maps
- * },
- * detail: {
- *  itemArray: new LoopMap({
- *    position: 0,
- *    values: {
- *    itemCode: new FieldMap({
- *    segmentIdentifier: "W07",
- *    identifierValue: null,
- *    identifierPosition: null,
- *    valuePosition: 4,
- *    }),
- *    lotCode: new FieldMap({
- *    segmentIdentifier: "W07",
- *    identifierValue: null,
- *    identifierPosition: null,
- *    valuePosition: 7,
- *    }),
- *    },
- *  })
- * }
- *};
+ * let itemArray: object = new LoopMap(
+ * 	0,	// position
+ * 	{	// values
+ * 		subscriberIndicator: new FieldMap("INS", null, null, 0),
+ * 		subscriberNumber: new FieldMap("REF", "0F", 0, 1)
+ * 	}
+ * );
+ * console.log(itemArray)
+ * // LoopMap {
+ * //   position: 0,
+ * //   values: {
+ * //     subscriberIndicator: FieldMap {
+ * //       segmentIdentifier: 'INS',
+ * //       identifierValue: null,
+ * //       identifierPosition: null,
+ * //       valuePosition: 0
+ * //     },
+ * //     subscriberNumber: FieldMap {
+ * //       segmentIdentifier: 'REF',
+ * //       identifierValue: '0F',
+ * //       identifierPosition: 0,
+ * //       valuePosition: 1
+ * //     }
+ * //   }
+ * // }
  */
 export default class LoopMap {
-	constructor(public position: number, public values: object) {	}
+	constructor(public position: number, public values: object) { }
 }
